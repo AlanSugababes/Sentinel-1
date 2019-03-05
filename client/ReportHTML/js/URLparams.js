1b6e26af32152237e1aac3f44394b9bcbce2a138
AlanSugababes <alansugababes@gmail.com>
2019-03-05 14:09:03 +0000

    Report page tested with URL params page

--- client/ReportHTML/js/URLparams.js
+++ client/ReportHTML/js/URLparams.js
@@ -1,3 +1,6 @@
 const urlParams = new URLSearchParams(location.search);
+const totalWordCount = urlParams.get('total word count')
 const words = urlParams.getAll('word') // Will store multiple words in an array
 const wordcount = urlParams.getAll('count') // Will store all word counts in an array
+// percentage
+let threatPercentage = (100 / totalWordCount) * totalThreatWords;
