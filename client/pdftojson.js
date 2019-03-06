d5f53f83c91f36f821fcb15e6793da18eaf9fd10
JClayton94 <joshclaytonmarrs@gmail.com>
2019-03-06 09:31:03 +0000

    new PDFtoJSON

--- client/pdftojson.js
+++ client/pdftojson.js
@@ -1,27 +1,14 @@
-const PDFParser = require("pdf2json");
+const pdf = require("pdf-parse");
 const fs = require("fs");
-PDFConverter();
+PDFtoJSONConverter();
 
-function PDFConverter(){
+function PDFtoJSONConverter(){
 
-let dir = JSON.parse(fs.readFileSync("./PDFConvertConfig.json", "utf8"));
-let dirFiles = fs.readdirSync(dir.pdfDir);
-let dirLength = dirFiles.length;
-let jsonDocName = dir.jsonRaw;
-var jsonDocArray = [];
-let pdf;
-
-for(let i = 0; i < dirLength; i++){
-    pdf  = new PDFParser();
-    pdf.on("pdfParser_dataError", errData => console.error("error" + errData.parserError));
-    pdf.on("pdfParser_dataReady", pdfData => {  
-            fs.writeFile(dir.jsonDir + jsonDocName + i +  ".json", JSON.stringify(pdfData), err => {
-                jsonDocArray.push(jsonDocName + i + ".json");      
-                if (err) throw err;
-            });
-        }); 
-    pdf.loadPDF(dir.pdfDir + dirFiles[i]);
-    }
+    let filePath = fs.readFileSync("C:\\Users\\Admin\\Desktop\\TestingParseer\\pdfs\\test1.pdf")
+    pdf(filePath).then(function(data) {
+        console.log(data.info);
+        console.log(data.metadata);
+        console.log(data.text);
+    });
 }
 
-module.exports = PDFConverter;
